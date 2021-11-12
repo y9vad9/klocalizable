c5219ef1e13c60b7db495bda446cc13796409605
y9neon <justneon33@gmail.com>
2021-11-12 23:22:12 +0200

    feat: updated algorithm of getting locale

--- src/commonMain/kotlin/KLocalizedDelegate.kt
+++ src/commonMain/kotlin/KLocalizedDelegate.kt
@@ -6,9 +6,20 @@ import kotlin.reflect.KProperty
 public class KLocalizedDelegate<T>(private val localized: KLocalized<T>, private val provider: CurrentLocalizationProvider) : ReadOnlyProperty<Any?, T> {
     override fun getValue(thisRef: Any?, property: KProperty<*>): T {
         val currentLocale = provider.provide()
-        return localized[currentLocale]
-            ?: localized[Locale.DEFAULT]
+        return chooseVariant(currentLocale)
             ?: throw IllegalStateException("Localized ${property.name} didn't have localization for $currentLocale. " +
                 "Unable to use Locale.DEFAULT as fallback, because it isn't specified.")
+    }
+
+    private fun chooseVariant(currentLocale: Locale): T? {
+        if(localized[currentLocale] != null)
+            return localized[currentLocale]!!
+
+        val available = localized.translations.keys
+        val langCodeMatches = available.filter { it.languageCode == currentLocale.languageCode }
+
+        return if(langCodeMatches.isNotEmpty())
+            localized[langCodeMatches.first()]
+        else localized[Locale.DEFAULT]
     }
 }
